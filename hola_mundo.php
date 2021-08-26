44cce6bdd2fdf237c65d50abc56e969c76037cd6
Val <valdo_jopa@hotmail.com>
2021-08-26 01:57:07 -0500

    hola mundo en PHP

---
+++ hola_mundo.php
@@ -0,0 +1,25 @@
+<?php
+// echo nos sirve para mostrar informacion en pantalla
+
+#otro comentario
+
+/* otro tipo de comentario */
+
+//echo "hola mundo";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta chararset="UFT-8">
+    <title>Document</title>
+</head>
+<body>
+    
+      <h1>hola mundo label h1</h1>
+
+      <h4> <?php echo "hola mundo" ?></h4>
+
+</body>    
+</html>    
