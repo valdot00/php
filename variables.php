1af114ae36483bc58a3fd8b5d1bd6fb9c4c76611
Val <valdo_jopa@hotmail.com>
2021-08-26 02:13:50 -0500

    variables en PHP

---
+++ variables.php
@@ -0,0 +1,27 @@
+<?php
+
+//string cadena de texto
+$nombre = "Jorge";
+
+//integer numeros enteros
+$numero = 8;
+
+//double numeros con decimales
+$numero_decimal=7.7;
+
+//boolean verdadero o falso (true / false)
+$verdadero = false;
+
+echo $nombre,$numero;
+
+//echo $numero;
+
+echo "hola, $nombre";#comillas dobles
+
+echo 'hola,'.$nombre;#comillas sencillas
+
+echo gettype($nombre);#tipo de dato gettype();
+
+echo gettype($numero);
+
+?>
