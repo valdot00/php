e9e82f3041d050cbd0d154dd25ff2d70fe94dd2a
Val <valdo_jopa@hotmail.com>
2021-08-26 10:27:15 -0500

    arreglos asosiativos en php

---
+++ arreglos_asosiativos.php
@@ -0,0 +1,13 @@
+<?php 
+
+$jorge = array('telefono' => '3334896208','edad' => 29,'apellido' => 'pelayo', 'pais' => 'mexico');
+
+echo $jorge['telefono'] . '<br/>';
+
+echo $jorge['edad'] . '<br/>';
+
+echo $jorge['apellido'] . '<br/>';
+
+echo $jorge['pais'] . '<br/>';
+
+?>
