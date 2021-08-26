24f66bab64ef4ac39d541a2e656f5550fc9e4ad0
Val <valdo_jopa@hotmail.com>
2021-08-26 11:17:08 -0500

    arreglos multidimencionales en php

---
+++ arreglos_multidimencionales.php
@@ -0,0 +1,13 @@
+<?php 
+
+$personas = array(
+    array('juan',1),
+    array('pepe',2),
+    array('ana',3)
+);
+
+echo $personas[0][0] . '<br/>';
+echo $personas[1][0] . '<br/>';
+echo $personas[2][0] . '<br/>';
+
+?>
